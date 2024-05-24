5a2c816b2d4a3f278a851df8e215acfd8cc8f8c6
andrianapryima <andrianap695@gmail.com>
2024-05-24 08:50:25 +0000

    Написано прототипи

---
+++ .ccls-cache/@home@runner@lab-12/sentence.h
@@ -0,0 +1,10 @@
+//прототипи
+#pragma once
+#include <string>
+using namespace std;
+extern string word, line;
+
+inputData();
+void RemoveWord (string &str, const string &word);
+int countWords (const string &line);
+
--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
+/*Написати програму, яка у заданому текстовому файлі видаляє введене з клавіатури слово та повертає кількість видалених слів.*/
+
 #include <iostream>
 #include "sentence.h"
 
 using namespace std;
-int main(){
 
+extern string word, line;
+
+int main()
+{
+  inputData();
+  RemoveWord (&str, string &word);
+  cout<< countWords(&line)<< "Result: "<< line << endl;
+    
   return 0;
 }
